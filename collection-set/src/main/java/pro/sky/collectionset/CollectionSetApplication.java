ee571d66495547e02da25f172f33fa06848da936
Sergey <enotagressor@gmail.com>
2022-06-24 15:35:17 +0700

    1

---
+++ collection-set/src/main/java/pro/sky/collectionset/CollectionSetApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionSetApplication.class, args);
+	}
+
+}
